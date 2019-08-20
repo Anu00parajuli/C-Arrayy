ecac43d968421a581c9ceb4cd24427e2e45646e3
Anushka Parajuli <46156997+Anu00parajuli@users.noreply.github.com>
2019-08-20 08:58:52 +0545

    Add files via upload

---
+++ finding simple intrest by passing default argument rate in parameterized constructor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class findintrest{
+	int principle,rate,time;
+	public:
+		findintrest(int a,int b,int c=12){
+			principle=a;
+			time=b;
+			rate=c;
+			cout<<"The required simple intrest is "<<principle*rate*time<<endl;
+		}
+};
+int main()
+{
+	findintrest t1(1000,200);
+	return 0;
+}
